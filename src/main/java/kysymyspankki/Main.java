575609de210711584ab239f57dff978eacdfbd03
Kallioinen Miika R I <kamika@da5-cs-b221-25.pc.helsinki.fi>
2018-10-11 17:19:40 +0300

    make it better

--- src/main/java/kysymyspankki/Main.java
+++ src/main/java/kysymyspankki/Main.java
@@ -4,8 +4,11 @@ package kysymyspankki;
 import spark.Spark;
 
 public class Main {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
         Spark.get("*", (req, res) -> {
             return "Hei maailma!";
         });
