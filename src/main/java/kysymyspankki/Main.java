1fec3d6fac978e945cd915d3ccb62e60e1192996
Kallioinen Miika R I <kamika@da5-cs-b221-25.pc.helsinki.fi>
2018-10-11 18:08:04 +0300

    make it butter

--- src/main/java/kysymyspankki/Main.java
+++ src/main/java/kysymyspankki/Main.java
@@ -1,17 +1,31 @@
 
 package kysymyspankki;
 
-import spark.Spark;
+import spark.*;
+import java.sql.*;
+import java.util.*;
+import spark.template.thymeleaf.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
-        Spark.get("*", (req, res) -> {
-            return "Hei maailma!";
-        });
+        Spark.get("/*", (req, res) -> {
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
 
     }
+    
+    public static Connection getConnection() throws Exception {
+    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+    if (dbUrl != null && dbUrl.length() > 0) {
+        return DriverManager.getConnection(dbUrl);
+    }
+
+    return DriverManager.getConnection("jdbc:sqlite:kysymyspankki.db");
+}
 }
