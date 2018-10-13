df53433bb05fd582bed8938f31f73b497695dfb4
KalliMiika <miika.kallioinen@helsinki.fi>
2018-10-14 02:16:48 +0300

    uus yrkkä

--- src/main/java/kysymyspankki/database/Database.java
+++ src/main/java/kysymyspankki/database/Database.java
@@ -13,7 +13,7 @@ public class Database {
         this.databaseAddress = databaseAddress;
     }
 
-    public Connection getConnection() throws SQLException {
+    public Connection getConnection() throws Exception {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         if (dbUrl != null && dbUrl.length() > 0) {
             return DriverManager.getConnection(dbUrl);
