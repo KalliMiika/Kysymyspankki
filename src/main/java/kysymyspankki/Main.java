2d675965f8f468e33f973da6f43d189891ac3ded
KalliMiika <miika.kallioinen@helsinki.fi>
2018-10-13 19:45:53 +0300

    tietokannan lisäys

--- src/main/java/kysymyspankki/Main.java
+++ src/main/java/kysymyspankki/Main.java
@@ -4,11 +4,13 @@ package kysymyspankki;
 import spark.*;
 import java.sql.*;
 import java.util.*;
+import kysymyspankki.database.Database;
 import spark.template.thymeleaf.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        Database database = getDatabase();
         if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
@@ -19,13 +21,12 @@ public class Main {
         }, new ThymeleafTemplateEngine());
 
     }
-    
-    public static Connection getConnection() throws Exception {
-    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-    if (dbUrl != null && dbUrl.length() > 0) {
-        return DriverManager.getConnection(dbUrl);
-    }
 
-    return DriverManager.getConnection("jdbc:sqlite:kysymyspankki.db");
-}
+    public static Database getDatabase() throws Exception {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if (dbUrl != null && dbUrl.length() > 0) {
+            return new Database(dbUrl);
+        }
+        return new Database("jdbc:sqlite:kysymyspankki.db");
+    }
 }
